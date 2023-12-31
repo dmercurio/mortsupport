58e50832980acb9f9d9888b74158bc76d66269f6
David Mercurio <dave.mercurio@gmail.com>
2023-12-30 23:07:38 -0800

    add server with status check endpoint

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+import compression from 'compression';
+import express, {Router} from 'express';
+import helmet from 'helmet';
+
+const router = Router();
+
+const statusCheck = JSON.parse(fs.readFileSync('../infrastructure/outputs/staging/statusCheck.json', 'utf8'));
+router.get(statusCheck.path, async(request, response) => {
+  response.send(statusCheck.content);
+});
+
+const app = express();
+app.use(compression());
+app.use(helmet());
+app.use("/", router);
+app.listen(8080, () => {
+  console.log('server started on http://localhost:8080')
+});
