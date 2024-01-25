c30ccfd2218cb123fc7f99202f558fe80659d7bf
David Mercurio <dave.mercurio@gmail.com>
2024-01-24 17:12:17 -0800

    return an upload url

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,8 @@ import * as fs from 'fs';
 import compression from 'compression';
 import express, {Router} from 'express';
 import helmet from 'helmet';
+import time from './lib/time';
+import {storage} from './lib/environment';
 
 const router = Router();
 
@@ -9,6 +11,21 @@ const router = Router();
 const statusCheck = JSON.parse(fs.readFileSync('../infrastructure/outputs/staging/statusCheck.json', 'utf8'));
 router.get(statusCheck.path, async (request, response) => {
   response.send(statusCheck.content);
+});
+
+const BUCKET = `${process.env.GOOGLE_CLOUD_PROJECT}-bucket`;
+
+router.get('/api/upload-url/:documentId', async (request, response) => {
+  const document = {id: request.params.documentId}; // TODO this is just a stub - load the object from the documentId
+
+  await storage
+    .bucket(BUCKET)
+    .file(`${document.id}.jpg`) // TODO prefix with clientId
+    .getSignedUrl({
+      version: 'v4',
+      action: 'read',
+      expires: Date.now() + time.milliseconds({hours: 1}),
+    });
 });
 
 const app = express();
