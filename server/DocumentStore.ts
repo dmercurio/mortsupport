dd2a3657a12dd149be496356826946fc3558d40f
David Mercurio <dave.mercurio@gmail.com>
2024-01-25 17:16:40 -0800

    use secure random ids for documents

--- server/DocumentStore.ts
+++ server/DocumentStore.ts
@@ -1,7 +1,9 @@
 import {AnyoneCanView} from './lib/viewpolicy';
 import {Model, Store} from './lib/model';
+import {secureRandomId} from './lib/ids';
 
 export class Document extends Model {
+  id: string = secureRandomId();
   complete?: number = 0;
 }
 export const DocumentStore = Store<Document>(Document, 'documents', AnyoneCanView);
