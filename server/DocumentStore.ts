9ed6c76e7cde6f04c5d0e02439d933d348f6dc3e
David Mercurio <dave.mercurio@gmail.com>
2024-03-08 11:51:30 -0800

    add pii decorator to Documents

--- server/DocumentStore.ts
+++ server/DocumentStore.ts
@@ -1,5 +1,5 @@
 import {AnyoneCanView} from './lib/viewpolicy';
-import {Model, Store} from './lib/model';
+import {Model, Store, pii} from './lib/model';
 import {secureRandomId} from './lib/ids';
 
 export type DocumentStatus = 'WAITING' | 'VERIFYING' | 'SUCCESS' | 'FAILURE';
@@ -7,12 +7,12 @@ export type DocumentStatus = 'WAITING' | 'VERIFYING' | 'SUCCESS' | 'FAILURE';
 export class Document extends Model {
   id: string = secureRandomId();
   filename?: string;
-  name!: string;
-  birthdate!: string;
-  deathdate!: string;
-  ssnLast4?: string;
+  @pii name!: string;
+  @pii birthdate!: string;
+  @pii deathdate!: string;
+  @pii ssnLast4?: string;
   status?: DocumentStatus = 'WAITING';
-  fields?: Record<string, string> = {};
+  @pii fields?: Record<string, string> = {};
   mimetype?: string;
 }
 export const DocumentStore = Store<Document>(Document, 'documents', AnyoneCanView);
