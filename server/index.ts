cefbfb65afb7d4f1422c9e12dad4f368fdf0b057
David Mercurio <dave.mercurio@gmail.com>
2024-03-08 13:06:24 -0800

    add status message

--- server/index.ts
+++ server/index.ts
@@ -117,9 +117,11 @@ taskHandlers.post('/verify-document', async (request, response) => {
   const normalizeSSN = (ssn: string) => ssn.replaceAll(/[^0-9]/g, '').slice(-4);
 
   let birthdateVerified = false;
-  let firstNameVerified = false,
-    lastNameVerified = false;
+  let firstNameVerified = false;
+  let lastNameVerified = false;
   let ssnVerified = false;
+  let deathdateVerified = false;
+  let certificateVerified = false;
   const expectedNameParts = normalizeName(documentObj.name);
   const parsedFields: Record<string, string> = {};
   for (let field of fields) {
@@ -139,11 +141,27 @@ taskHandlers.post('/verify-document', async (request, response) => {
     } else if (field?.fieldName?.match(/social.*security|social.*number|ssn/i) && !ssnVerified) {
       parsedFields.ssnLast4 = normalizeSSN(field.fieldValue);
       ssnVerified = parsedFields.ssnLast4 === documentObj.ssnLast4;
+    } else if (field?.fieldName?.match(/death.*date|date.*death/i) && !deathdateVerified) {
+      const deathdate = chrono.parseDate(field.fieldValue)?.toLocaleDateString('en-US', DATE_FORMAT) || '';
+      parsedFields.deathdate = deathdate;
+      deathdateVerified = parsedFields.deathdate === documentObj.deathdate;
     }
   }
   const status: DocumentStatus =
-    birthdateVerified && firstNameVerified && lastNameVerified && ssnVerified ? 'SUCCESS' : 'FAILURE';
-  await DocumentStore.update({}, documentObj.id, {status: status, fields: parsedFields});
+    birthdateVerified && firstNameVerified && lastNameVerified && ssnVerified && deathdateVerified ? 'SUCCESS' : 'FAILURE';
+  let statusMessage = '';
+  if (!firstNameVerified || !lastNameVerified) {
+    statusMessage = 'Name mismatch';
+  } else if (!birthdateVerified) {
+    statusMessage = 'Birthdate mismatch';
+  } else if (!ssnVerified) {
+    statusMessage = 'SSN mismatch';
+  } else if (!deathdateVerified) {
+    statusMessage = 'Date of death mismatch';
+  } else if (!certificateVerified) {
+    statusMessage = 'Unable to verify certificate';
+  }
+  await DocumentStore.update({}, documentObj.id, {status, statusMessage, fields: parsedFields});
 
   response.send();
 });
