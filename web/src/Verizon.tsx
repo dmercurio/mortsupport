feb85e5132b99d1c0c309e4fc73800704eb4d33d
David Mercurio <dave.mercurio@gmail.com>
2024-03-12 11:40:38 -0700

    add remaining customers

--- web/src/Verizon.tsx
+++ web/src/Verizon.tsx
@@ -10,10 +10,36 @@ const API_PATH = process.env.REACT_APP_API_PATH;
 const POLLING_INTERVAL_SECONDS = 5;
 
 const customers: [Customer, string][] = [
-  [{name: 'Jill Diane Harmon', birthdate: '11/11/1962', last4SSN: '7034'}, 'DC Colorado.pdf'],
-  [{name: 'Francis Baird Hutto Jr', birthdate: '06/25/1926', last4SSN: '6754'}, 'DC Colorado2.pdf'],
-  [{name: 'Melvin Lipman', birthdate: '08/12/1928', last4SSN: ''}, 'DC Connecticut 2008.pdf'],
-  [{name: 'Dorothy H Feigin', birthdate: '03/05/1932', last4SSN: ''}, 'DC Connecticut.pdf'],
+  [{name: 'Jill Diane Harmon', birthdate: '11/11/1962', last4SSN: '7034'}, 'DC Colorado.pdf, 08/16/2021'],
+  [{name: 'Francis Baird Hutto Jr', birthdate: '06/25/1926', last4SSN: '6754'}, 'DC Colorado2.pdf, 08/06/2023'],
+  [{name: 'Melvin Lipman', birthdate: '08/12/1928', last4SSN: ''}, 'DC Connecticut 2008.pdf, 10/25/2008'],
+  [{name: 'Dorothy H Feigin', birthdate: '03/05/1932', last4SSN: ''}, 'DC Connecticut.pdf, 03/08/2023'],
+  [{name: 'George Edwin Coffey', birthdate: '11/24/1929', last4SSN: '6647'}, 'DC Kentucky.pdf, 06/08/2023'],
+  [{name: 'Thomas Paul Pietras', birthdate: '11/15/1934', last4SSN: '8050'}, 'DC Michigan.pdf, 06/15/2023'],
+  [{name: 'Roberta Kay Stipcich', birthdate: '05/05/1935', last4SSN: '4417'}, 'DC Montana.pdf, 06/10/2023'],
+  [{name: 'James Robert Solomon', birthdate: '06/05/1958', last4SSN: '7779'}, 'DC New Jersey.pdf, 11/28/2022'],
+  [{name: 'William Conrad Knodel', birthdate: '08/19/1938', last4SSN: ''}, 'DC OHIO -- William Knodel.pdf, 05/26/2023'],
+  [
+    {name: 'Elizabeth M Knodel', birthdate: '06/05/1936', last4SSN: '7364'},
+    'DC OHIO -- Elizabeth Knodel.pdf, 05/29/2023',
+  ],
+  [{name: 'Early Bunts ', birthdate: '02/25/1928', last4SSN: ''}, 'DC Ohio Bunts.pdf, 09/17/2023'],
+  [{name: 'Daniel R Duchanois', birthdate: '10/23/1954', last4SSN: ''}, 'DC Ohio Duchanois.pdf, 10/09/2023'],
+  [{name: 'Robert K Lampert', birthdate: '12/09/1950', last4SSN: ''}, 'DC Ohio Lampert.pdf, 10/26/2023'],
+  [{name: 'Violet Spevack', birthdate: '07/15/1916', last4SSN: ''}, 'DC Ohio Spevack.pdf, 09/07/2023'],
+  [
+    {name: 'George K. Argyros', birthdate: '04/18/1936', last4SSN: '1192'},
+    'DC Pennsylvania certified 2010 stray marks..., 04/26/2010',
+  ],
+  [
+    {name: 'George K. Argyros', birthdate: '04/18/1936', last4SSN: '1192'},
+    'DC Pennsylvania certified 2010.pdf, 04/26/2010',
+  ],
+  [
+    {name: 'Vangie Argyros', birthdate: '09/08/1942', last4SSN: '4197'},
+    'DC Pennsylvania noncertified 2023.pdf, 04/24/2023',
+  ],
+  [{name: 'Ruth M Franklin', birthdate: '09/17/1930', last4SSN: ''}, 'Ohio DC Franklin.pdf, 08/30/2023'],
 ];
 
 type Customer = {
